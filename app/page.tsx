f3c30ca523896494051a6dad55d03ca2fcb2bc62
Ruei <ruei@null.computer>
2023-06-16 00:04:52 +0800

    update: update style for title and seach button hover style

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import SearchCompany from '#/components/SearchCompany';
 
 export default function Home() {  
   return (
-    <div className="pt-14 mx-auto max-w-md">
-      <h1 className="mb-4 py-4 text-center text-5xl font-bold tracking-widest">博奕守護者</h1>
+    <div className="mx-auto max-w-md">
+      <h1 className="mb-6 py-4 text-center text-5xl font-bold tracking-widest">博奕守護者</h1>
 
       <SearchCompany />
     </div>
--- components/SearchCompany/SearchCompany.tsx
+++ components/SearchCompany/SearchCompany.tsx
@@ -18,7 +18,7 @@ export default function SearchCompany() {
       <div className="mb-6 h-12 flex">
         <input className="p-2 h-full text-lg flex-1 border border-slate-300 rounded-tl rounded-bl outline-blue-500" placeholder="統邊或公司名稱" ref={ref} />
 
-        <button className="p-2 w-12 h-12 flex items-center justify-center bg-blue-600 text-white rounded-tr rounded-br" onClick={handleSearch}>
+        <button className="p-2 w-12 h-12 flex items-center justify-center bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded-tr rounded-br" onClick={handleSearch}>
           <SearchIcon />
         </button>
       </div>
