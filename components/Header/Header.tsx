a4ba4b3af22a08af9a22e73b56d4f524c54468ae
Ruei <ruei@null.computer>
2023-06-20 00:38:33 +0800

    update: hide request button on /request page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,20 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import RequestButton from '#/components/RequestButton';
 
 export default function Header() {
+  const path = usePathname();
+  const showRequestButton = path === '/';
+
   return (
     <header className="px-8 py-4 h-[80px] flex items-center justify-end">
-      <Link href="/request">
-        <RequestButton />
-      </Link>
+      {showRequestButton && (
+        <Link href="/request">
+          <RequestButton />
+        </Link>
+      )}
     </header>
   );
 }
